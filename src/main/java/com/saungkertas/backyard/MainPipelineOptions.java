e4ff2e3360d3aa3ccc4eb0269b59711c6cb13d7a
syarifhidayatullah <syarif.h@go-jek.com>
2018-09-15 22:17:59 +0700

    Created Main Pipeline Options for user input

---
+++ src/main/java/com/saungkertas/backyard/MainPipelineOptions.java
@@ -0,0 +1,27 @@
+package com.saungkertas.backyard;
+
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.options.ValueProvider;
+
+public interface MainPipelineOptions extends PipelineOptions {
+
+    @Description("Kafka Brokers")
+    @Validation.Required
+    String getKafkaBrokers();
+
+    void setKafkaBrokers(ValueProvider<String> KafkaBrokers);
+
+    @Description("Kafka Topics")
+    @Validation.Required
+    String getKafkaTopic();
+
+    void setKafkaTopic(ValueProvider<String> kafkaTopic);
+
+    @Description("DownStream GCS")
+    @Validation.Required
+    String getDownstreamGcs();
+
+    void setDownstreamGcs(ValueProvider<String> downstreamGcs);
+}
